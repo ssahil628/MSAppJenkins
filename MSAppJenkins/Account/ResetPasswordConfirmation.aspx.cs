1b79c429f8a3998b4453d8be4be50b8b22c48369
Sahil <ssahil628@gmail.com>
2018-09-19 15:20:47 +0530

    Files Added

---
+++ MSAppJenkins/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MSAppJenkins.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MSAppJenkins/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSAppJenkins.Startup))]
+namespace MSAppJenkins
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
